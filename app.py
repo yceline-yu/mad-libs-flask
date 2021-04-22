7a52660f5da355a5fc065db7c2fa659d8a16288b
yceline-yu <cellystuff5@gmail.com>
2021-04-21 18:27:02 -0700

    further study

--- app.py
+++ app.py
@@ -2,18 +2,31 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+from stories import excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+STORIES = {
+    "silly_story": silly_story,
+    "excited_story": excited_story
+}
+
 @app.route("/")
 def home():
-    """retuns the homepage with all the word inputs labeled"""
-    form_prompts = silly_story.prompts
+    """retuns the homepage menu for story picking"""
+    template_stories = ["silly_story", "excited_story"]
 
-    return render_template("questions.html", prompts = form_prompts)
+    return render_template("homepage.html", story_list = template_stories)
+
+@app.route("/story/<story_temp>")
+def picked_story(story_temp):
+    """retuns the page with all the word inputs labeled"""
+    form_prompts = STORIES[story_temp].prompts
+
+    return render_template("questions.html", prompts = form_prompts)    
 
 @app.route("/results")
 def results():
